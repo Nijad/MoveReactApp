98adcf459a8207119c393c2ccc25b3994466e14b
khourynj <nijad-k@live.com>
2025-01-15 13:50:21 +0300

    add packages

--- movereactapp.client/src/pages/Layout.jsx
+++ movereactapp.client/src/pages/Layout.jsx
@@ -1,5 +1,8 @@
 import { Outlet, Link } from "react-router-dom";
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 const Layout = () => {
   return (
     <>
