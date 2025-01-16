2f503a95a77d84edc50789e826d9ff9a3135f775
khourynj <nijad-k@live.com>
2025-01-16 14:03:38 +0300

    app bar

--- movereactapp.client/src/App.jsx
+++ movereactapp.client/src/App.jsx
@@ -3,23 +3,18 @@ import Layout from "./pages/layout";
 import Departments from "./pages/Departments/Departments";
 import Extensions from "./pages/Extensions/Extensions";
 import Configurations from "./pages/Configurations/configurations";
+import NoPage from "./pages/NoPage/NoPage";
+import Index from "./pages/Index/Index";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route
-          index
-          element={
-            <div>
-              <h1>index</h1>
-            </div>
-          }
-        />
+        <Route index element={<Index />} />
         <Route path="Departments" element={<Departments />} />
         <Route path="Extensions" element={<Extensions />} />
         <Route path="Configurations" element={<Configurations />} />
-        {/* <Route path="*" element={<NoPage />} /> */}
+        <Route path="*" element={<NoPage />} />
       </Route>
     </Routes>
   );
--- movereactapp.client/src/pages/Layout.jsx
+++ movereactapp.client/src/pages/Layout.jsx
@@ -1,29 +1,45 @@
-import { Outlet, Link } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { AppBar, Box, Button, Stack, Toolbar } from "@mui/material";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Departments">Departments</Link>
-          </li>
-          <li>
-            <Link to="/Extensions">Extensions</Link>
-          </li>
-          <li>
-            <Link to="/Configurations">Configurations</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
+      <AppBar>
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Button
+              variant="text"
+              href="/"
+              color="inherit"
+              startIcon={<FileCopyIcon />}
+            >
+              Move App
+            </Button>
+          </Box>
+          <Stack direction="row" spacing={2}>
+            <Button variant="text" href="/departments" color="inherit">
+              Departments
+            </Button>
+            <Button variant="text" href="/extensions" color="inherit">
+              Extensions
+            </Button>
+            <Button variant="text" href="/configurations" color="inherit">
+              Configurations
+            </Button>
+            <Button variant="text" href="#" color="inherit">
+              Move Manually
+            </Button>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      <Box marginY={10} marginX={3}>
+        <Outlet />
+      </Box>
     </>
   );
 };
