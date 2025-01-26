083cebf8cfbc4b189a57548e65b5998629173915
Nijad Alkhoury <nijad-k@live.com>
2025-01-26 20:28:54 +0300

    new layout

--- movereactapp.client/src/pages/Extensions/Extensions_new.jsx
+++ movereactapp.client/src/pages/Extensions/Extensions_new.jsx
@@ -6,11 +6,19 @@ import React from "react";
 
 function Extensions_new() {
   return (
-    <Stack border="1px solid">
-      <Box>
-        <Typography width="grow">sidebar</Typography>
-      </Box>
-    </Stack>
+    <Grid2 container direction="row">
+      <Grid2 size={3} border="1px solid" height="100vh">
+        <Typography>sidebar</Typography>
+      </Grid2>
+      <Grid2 size="grow" container direction="column">
+        <Grid2 border="1px solid" height="25%">
+          <Typography>header</Typography>
+        </Grid2>
+        <Grid2 border="1px solid" height="75%">
+          <Typography>main</Typography>
+        </Grid2>
+      </Grid2>
+    </Grid2>
   );
 }
 
