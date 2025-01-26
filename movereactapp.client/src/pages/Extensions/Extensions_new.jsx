e99bf805c4428d7ff91d2798ce1cc9630381b75a
Nijad Alkhoury <nijad-k@live.com>
2025-01-26 23:22:30 +0300

    new design

--- movereactapp.client/src/pages/Extensions/Extensions_new.jsx
+++ movereactapp.client/src/pages/Extensions/Extensions_new.jsx
@@ -1,20 +1,112 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-
-import { Box, Grid2, Stack, Typography } from "@mui/material";
+import ManageSearchIcon from "@mui/icons-material/ManageSearch";
+import { AccountCircle } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Card,
+  Divider,
+  Grid2,
+  IconButton,
+  InputBase,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 function Extensions_new() {
+  const ext = [
+    "doc",
+    "docx",
+    "xls",
+    "xlsx",
+    // "ppt",
+    // "pptx",
+    // "jpg",
+    // "jpeg",
+    // "png",
+    // "gif",
+    // "zip",
+    // "rar",
+    // "py",
+    // "cs",
+    // "vb",
+    // "js",
+    // "css",
+  ];
   return (
-    <Grid2 container direction="row">
-      <Grid2 size={3} border="1px solid" height="100vh">
-        <Typography>sidebar</Typography>
+    <Grid2 container direction="row" columns={100} height="calc(100vh - 64px)">
+      <Grid2
+        container
+        direction="column"
+        size={{ md: 25, lg: 18, xl: 15 }}
+        bgcolor="#1976d2"
+        height="calc(100vh - 64px)"
+      >
+        <Grid2
+          height="25%"
+          borderBottom="1px solid white"
+          display="flex"
+          flexDirection="column"
+          paddingX={2}
+          alignItems="center"
+          sx={{ justifyContent: "space-evenly" }}
+        >
+          <Button
+            sx={{
+              color: "#1976d2",
+              backgroundColor: "white",
+              width: "100%",
+            }}
+          >
+            Add New Extension
+          </Button>
+          <Paper
+            component="form"
+            sx={{
+              //p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              //marginX: 1,
+              //marginTop: 2,
+              //ml: 1,
+              //flexGrow: 1,
+              width: "100%",
+            }}
+          >
+            <ManageSearchIcon sx={{ ml: 1, color: "#1976d2" }} />
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="filter"
+              inputProps={{ "aria-label": "filter extension" }}
+            />
+          </Paper>
+        </Grid2>
+        <Grid2 height="75%" overflow="auto" sx={{ direction: "rtl" }}>
+          {ext.map((e) => (
+            <Box
+              key={e}
+              textAlign="center"
+              margin={1}
+              bgcolor="white"
+              color="#1976d2"
+              borderRadius={1}
+              boxShadow="10"
+              paddingY={0.5}
+            >
+              <Typography sx={{ fontWeight: 500 }}>{e}</Typography>
+            </Box>
+          ))}
+        </Grid2>
       </Grid2>
       <Grid2 size="grow" container direction="column">
-        <Grid2 border="1px solid" height="25%">
+        <Grid2 height="25%">
           <Typography>header</Typography>
         </Grid2>
-        <Grid2 border="1px solid" height="75%">
+        <Grid2 height="75%">
           <Typography>main</Typography>
         </Grid2>
       </Grid2>
--- movereactapp.client/src/pages/Layout.jsx
+++ movereactapp.client/src/pages/Layout.jsx
@@ -1,12 +1,54 @@
-import { AppBar } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { AppBar, Box, Stack, Toolbar } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+import DriveFileMoveIcon from "@mui/icons-material/DriveFileMove";
+import FilterIcon from "@mui/icons-material/Filter";
+import LineStyleIcon from "@mui/icons-material/LineStyle";
+import HomeIcon from "@mui/icons-material/Home";
 import { Outlet } from "react-router-dom";
+import AppBarLink from "../components/AppBarLink";
 
-function Layout() {
+const Layout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <Box height="100vh">
+      <AppBar>
+        <Toolbar style={{ padding: "0" }}>
+          <Stack
+            direction="row"
+            spacing={0}
+            sx={{ flexGrow: 1 }}
+            alignItems="center"
+          >
+            <AppBarLink title="HOME" link="/" icon={<HomeIcon />} />
+          </Stack>
+          <Stack direction="row" spacing={0}>
+            <AppBarLink
+              title="DEPARTEMENTS"
+              link="/departments"
+              icon={<LineStyleIcon />}
+            />
+            <AppBarLink
+              title="EXTENSIONS"
+              link="/extensions"
+              icon={<FilterIcon />}
+            />
+            <AppBarLink
+              title="CONFIG"
+              link="/configurations"
+              icon={<SettingsIcon />}
+            />
+            <AppBarLink title="MOVE" link="#" icon={<DriveFileMoveIcon />} />
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      <Box top={64} position="relative">
+        <Outlet />
+      </Box>
+    </Box>
   );
-}
+};
 
 export default Layout;
