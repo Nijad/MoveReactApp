b319fd4618a7dbaa28375d212b99be40040fb9d6
khourynj <nijad-k@live.com>
2025-02-16 15:07:06 +0300

    install folder tree

---
+++ MoveReactApp.Server/Controllers/MoveController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoveReactApp.Server.Database;
+using MoveReactApp.Server.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MoveReactApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoveController : ControllerBase
+    {
+        Operations operations = new();
+        // GET: api/<MoveController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<Department> depts = operations.GetDepartments();
+            return ["sd","df"];
+        }
+
+        // GET api/<MoveController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MoveController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MoveController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MoveController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- movereactapp.client/src/pages/Move/Move.jsx
+++ movereactapp.client/src/pages/Move/Move.jsx
@@ -1,5 +1,86 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+
+import { Grid2 } from "@mui/material";
+import FolderTree, { testData } from "react-folder-tree";
+
 function Move() {
-  return <div>Move Page</div>;
+  const treeState = {
+    name: "root",
+    isOpen: false,
+    children: [
+      { name: "children 1", checked: 0 },
+      {
+        name: "children 2",
+        isOpen: false,
+        children: [
+          { name: "children 2-1", checked: 0 },
+          { name: "children 2-2", checked: 1 },
+        ],
+      },
+      {
+        name: "folder 3",
+        children: [
+          {
+            name: "folder 3",
+            children: [
+              {
+                name: "folder 3",
+                children: [
+                  {
+                    name: "folder 3",
+                    children: [],
+                    isOpen: false,
+                  },
+                  {
+                    name: "folder 3",
+                    children: [],
+                    isOpen: false,
+                  },
+                ],
+                isOpen: false,
+              },
+            ],
+            isOpen: false,
+          },
+        ],
+        isOpen: false,
+      },
+    ],
+  };
+  const onTreeStateChange = (state, event) => console.log(state, event);
+  return (
+    <Grid2
+      container
+      direction="row"
+      columns={100}
+      minHeight="calc(100vh - 64px)"
+    >
+      <Grid2
+        container
+        direction="column"
+        size={{ md: 30, lg: 25, xl: 20 }}
+        bgcolor="#1976d2"
+        color="white"
+        padding={2}
+        fontSize="1.1em"
+        sx={{ overflowX: "auto" }}
+      >
+        <FolderTree
+          showCheckbox={false}
+          data={treeState}
+          onChange={onTreeStateChange}
+          readOnly
+          onNameClick={(d) => {
+            console.log(d.nodeData);
+          }}
+        />
+      </Grid2>
+      <Grid2 size="grow" container direction="column" margin={2} spacing={2}>
+        Files will display here
+      </Grid2>
+    </Grid2>
+  );
 }
 
 export default Move;
